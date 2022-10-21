c0d45aeda361aee23952cdea8acd144b2341fcd5
suxiongwei <suxiongwei@kaoshixing.com>
2022-10-21 21:31:38 +0800

    feat: 多数元素

---
+++ src/main/java/com/sxw/learn/leetcode/array/MajorityElement.java
@@ -0,0 +1,18 @@
+package com.sxw.learn.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * [题目]: 多数元素(169)
+ * [题目描述]:
+ *
+ * [解题思路]:
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);// 排序后 找众数
+        return nums[nums.length / 2];
+    }
+}
