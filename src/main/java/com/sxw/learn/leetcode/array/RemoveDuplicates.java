6b23d83ea1863b6413e911838e9474b20de666c3
suxiongwei <suxiongwei@smzdm.com>
2023-01-12 14:17:06 +0800

    题目：去掉重复元素

--- src/main/java/com/sxw/learn/leetcode/array/RemoveDuplicates.java
+++ src/main/java/com/sxw/learn/leetcode/array/RemoveDuplicates.java
@@ -21,8 +21,41 @@ public class RemoveDuplicates {
         return i + 1;// 因为快指针是从1开始的 而且至少也有一个
     }
 
+    /**
+     * 给你一个排序数组 nums，在逻辑上删除其中的重复元素，返回新的数组的长度 len，使得原数组 nums 的前 len 个元素中，每个数字最多出现两次。
+     *
+     * 如果一个数字出现超过2次，则这个数字最后保留两个。
+     *
+     * 1,1,1,2,2,3 -> 5
+     */
+    public static int removeDuplicates1(int[] nums) {
+        int res = 0;
+        if (nums == null) return res;
+        int length = nums.length;
+        if(length == 0) return res;
+        // 去重后元素的下标
+        int index = 0;
+        // 当前元素出现的次数
+        int count = 1;
+        for (int i = 1; i < length; i++){
+            // 和已有添加的数据做比较
+            if (nums[i] == nums[index]){
+                count++;
+                if (count <= 2){
+                    nums[++index] = nums[i];
+                }
+            }else {
+                nums[++index] = nums[i];
+                count = 1;
+            }
+        }
+        return index + 1;
+    }
+
     public static void main(String[] args) {
-        int i = RemoveDuplicates.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
+//        int i = RemoveDuplicates.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
+//        System.out.println(i);
+        int i = RemoveDuplicates.removeDuplicates1(new int[]{1,1,1,2,2,3});
         System.out.println(i);
     }
 }
